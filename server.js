462651e268f83ba74932101412af269d89ae72b5
Salihu Aminu <yakudimajnr@gmail.com>
2021-02-18 12:04:49 +0400

    configure root path set port for app.listen

--- server.js
+++ server.js
@@ -20,3 +20,12 @@ let db
 MongoClient.connect('mongodb+srv://procoderwalid:yakudima081aminu@cluster0.rqabd.mongodb.net/test', (err, client)=>{
     db = client.db('webstore')
 })
+
+//display a message or root path to show that API is working
+app.get('/', (req, res, next) => {
+    res.send('Select a collection, e.g., collection/messages')
+})
+
+app.listen(port, ()=>{
+    console.log('Express js server runnning')
+})
