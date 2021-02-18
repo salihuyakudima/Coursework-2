f0f467301aed85342dcca354b4b03a22188702b3
Salihu Aminu <yakudimajnr@gmail.com>
2021-02-18 11:56:13 +0400

    configure express js

--- server.js
+++ server.js
@@ -4,3 +4,12 @@ const { ObjectID } = require('mongodb')
 
 //create an express js instance
 const app = exp()
+
+//config express js
+app.use(exp.json())
+const port = process.env.PORT || 3000
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Headers", "*");
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next()
+})
